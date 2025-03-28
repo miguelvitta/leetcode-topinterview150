1b89953013a9532e6bed05a0607c8f83299fa6d8
miguelvitta <miguelvittadev@gmail.com>
2025-03-27 22:20:03 -0300

    this solution passes the 3 test cases but when submitted it was rejected by stricter tests

--- merge-sorted-array-88.c
+++ merge-sorted-array-88.c
@@ -39,16 +39,17 @@ nums2.length == n
 Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 */
 
-
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     int len = m + n;
-    for (int i = len - 1, j = m - 1, k = n - 1; i > 0; i--) {
+    for (int i = len - 1, j = m - 1, k = n - 1; i >= 0; i--) {
         if ((j > 0) && (nums1[j] >= nums2[k])) {
             nums1[i] = nums1[j];
             j--;
         } else if ((k > 0) && (nums2[k] >= nums1[j])) {
             nums1[i] = nums2[k];
             k--;
+        } else if (m == 0) {
+            nums1[i] = nums2[k];
         }
     }
 }
