7d23b9bae0061e4b9dbc77cce4f89fa1538bd8df
miguelvitta <miguelvittadev@gmail.com>
2025-03-29 09:56:39 -0300

    feature: a way better and simpler solution that actually passed all the tests

--- merge-sorted-array-88.c
+++ merge-sorted-array-88.c
@@ -93,17 +93,19 @@ int main() {
 }
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int len = m + n;
-    for (int i = len - 1, j = m - 1, k = n - 1; i >= 0; i--) {
-        if ((j > 0) && (nums1[j] >= nums2[k])) {
+    int i = m + n - 1;
+    int j = m - 1;
+    int k = n - 1;
+    while (k >= 0) {
+        if ((j >= 0) &&  (nums1[j] > nums2[k])) {
             nums1[i] = nums1[j];
             j--;
-        } else if ((k > 0) && (nums2[k] >= nums1[j])) {
+        }
+        else {
             nums1[i] = nums2[k];
             k--;
-        } else if (m == 0) {
-            nums1[i] = nums2[k];
         }
+        i--;
     }
 }
 
